5eed560dc3c31e699e0b19e8de26f2c56972182f
Frank Cardillo <facardillo91@gmail.com>
2016-09-29 13:04:18 -0400

    hooking up api and responders gem to current app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
@@ -7,7 +12,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(users)
-    pages_path
+    api_v1_pages_path
   end
 
   def after_sign_out_path_for(users)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   as :user do
-    get '/' => 'pages#index'
+    get '/' => 'api/v1/pages#index'
   end
   namespace :api do
     namespace :v1 do
