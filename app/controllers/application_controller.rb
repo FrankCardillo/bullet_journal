c4aa6b6e2cb486d4e9856b04b9b42eb2a89fb106
Frank Cardillo <facardillo91@gmail.com>
2016-10-15 18:27:32 -0400

    removed pages controller, routes, views, made entries top level

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(users)
-    api_v1_pages_path
+    api_v1_entries_path
   end
 
   def after_sign_out_path_for(users)
