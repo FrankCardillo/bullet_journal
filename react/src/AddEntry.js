744f1b509c2e1796db7af56e722d386cbaa4a078
Frank Cardillo <facardillo91@gmail.com>
2016-11-06 20:17:30 -0500

    began styling journal page

--- react/src/AddEntry.js
+++ react/src/AddEntry.js
@@ -6,19 +6,32 @@ import ReactDOM from 'react-dom';
 const AddEntry = (props) => {
   return (
         <form onSubmit={props.handleFormSubmit}>
-          <input
-            type="text"
-            placeholder="type"
-            value={props.type}
-            onChange={props.handleTypeChange}
-          />
-          <input
-            type="text"
-            placeholder="your entry"
-            value={props.body}
-            onChange={props.handleBodyChange}
-          />
-          <input type="submit" value="Add To List" />
+          <div className="row center">
+            <div className="small-12 column">
+              <input
+                type="text"
+                placeholder="type"
+                value={props.type}
+                onChange={props.handleTypeChange}
+                className="my-fields"
+              />
+            </div>
+            <div className="small-12 column">
+              <input
+                type="text"
+                placeholder="your entry"
+                value={props.body}
+                onChange={props.handleBodyChange}
+                className="my-fields"
+              />
+            </div>
+            <div className="small-12 column">
+              <input type="submit"
+                value="Add To List"
+                className="submit-button"
+              />
+            </div>
+          </div>
         </form>
   );
 }
