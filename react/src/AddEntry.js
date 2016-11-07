214e9ef15d83fa23b1c6621a78f962bd720e635b
Frank Cardillo <facardillo91@gmail.com>
2016-11-07 16:29:44 -0500

    first draft?? styling complete

--- react/src/AddEntry.js
+++ react/src/AddEntry.js
@@ -5,15 +5,15 @@ import ReactDOM from 'react-dom';
 
 const AddEntry = (props) => {
   return (
-        <form onSubmit={props.handleFormSubmit}>
-          <div className="row center">
+        <form onSubmit={props.handleFormSubmit} className="form-container">
+          <div className="row">
             <div className="small-12 column">
               <input
                 type="text"
                 placeholder="type"
                 value={props.type}
                 onChange={props.handleTypeChange}
-                className="my-fields"
+                className="entry-fields"
               />
             </div>
             <div className="small-12 column">
@@ -22,13 +22,13 @@ const AddEntry = (props) => {
                 placeholder="your entry"
                 value={props.body}
                 onChange={props.handleBodyChange}
-                className="my-fields"
+                className="entry-fields"
               />
             </div>
             <div className="small-12 column">
               <input type="submit"
                 value="Add To List"
-                className="submit-button"
+                className="all-buttons"
               />
             </div>
           </div>
